2cbb67420d7503b1bf07e0538e53b65de5f1e49e
snigo <snigo.ogins@gmail.com>
2021-02-03 16:00:24 +0100

    remove bootDB function from index

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bootDb = require('./model');
 const router = require('./routes');
 // require('dotenv').config();
 
@@ -10,9 +9,6 @@ const app = express();
 app.use(express.json());
 app.use('/api', router);
 
-(async () => {
-  await bootDb();
-  app.listen(port, () => {
-    console.log(`Running on port ${port}`);
-  });
-})();
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
