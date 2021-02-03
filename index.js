2df46e74babf1a89960da963185358c14274abe0
snigo <snigo.ogins@gmail.com>
2021-02-03 15:26:24 +0100

    bootstrap express app

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+// require('dotenv').config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
